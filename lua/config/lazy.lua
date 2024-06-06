f36190db1bb7d0c91b58fa393ec7a27bfc6b6d90
Enrico Krause <enrico.krause@gmail.com>
2024-06-06 08:55:25 +0200

    wip: disable prettier plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    --{ import = "lazyvim.plugins.extras.formatting.prettier" },
     --{ import = "lazyvim.plugins.extras.util.project" },
     --{ import = "lazyvim.plugins.extras.util.dot" },
 
